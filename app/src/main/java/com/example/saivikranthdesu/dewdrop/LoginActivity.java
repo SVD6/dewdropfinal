1e94ef7d9d4627078e66ef5919cfc6fb68c71d56
SVD6 <vikranthdesu@gmail.com>
2018-08-25 21:49:50 -0400

    Added two new activities and polished

--- app/src/main/java/com/example/saivikranthdesu/dewdrop/LoginActivity.java
+++ app/src/main/java/com/example/saivikranthdesu/dewdrop/LoginActivity.java
@@ -1,6 +1,24 @@
 package com.example.saivikranthdesu.dewdrop;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
 
+        TextView signup = findViewById(R.id.signup_button);
+        Button signin = findViewById(R.id.signin_button);
+
+        signin.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this, MainActivity.class)));
+        signup.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this, SignUpActivity.class)));
+
+    }
 }
--- app/src/main/java/com/example/saivikranthdesu/dewdrop/MainActivity.java
+++ app/src/main/java/com/example/saivikranthdesu/dewdrop/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.saivikranthdesu.dewdrop;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.support.v7.widget.CardView;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
